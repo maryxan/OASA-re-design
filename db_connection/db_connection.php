76f4c30254a713983aa2ecd55a7b0d843c69f4ea
GeorgeMitrakis <georgemitr96@hotmail.com>
2020-01-05 19:43:50 +0200

    meta

--- db_connection/db_connection.php
+++ db_connection/db_connection.php
@@ -9,5 +9,6 @@ $conn = mysqli_connect($host,$username,$password,$db);
 if($conn === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-
+$conn->query("SET NAMES 'utf8'");
+$conn->query("SET CHARACTER SET 'utf8'");
 ?>
