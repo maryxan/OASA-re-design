eff268a4bfbfd490ac511ea3918ef8176f31d610
GeorgeMitrakis <georgemitr96@hotmail.com>
2020-01-05 19:39:54 +0200

    meta

--- diadromi/diadromi.php
+++ diadromi/diadromi.php
@@ -8,7 +8,7 @@
 	<html lang="en">
 	<head>
 	<title>OASA.gr</title>
-	<meta charset="utf-8">
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
