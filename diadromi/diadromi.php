f9f8d6e34aa66cd4a6e5f53db8777dbf27dd1686
GeorgeMitrakis <georgemitr96@hotmail.com>
2020-01-05 19:09:09 +0200

    First route query

--- diadromi/diadromi.php
+++ diadromi/diadromi.php
@@ -1,75 +1,28 @@
-<?php
+	<?php
 
-  session_start();
+	include '../db_connection/db_connection.php';
+	session_start();
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>OASA.gr</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" type="text/css" href="diadromi.css">
+	?>
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<title>OASA.gr</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="diadromi.css">
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
-
-<style>
-  /* Style the form - display items horizontally */
-.form-inline {
-  display: flex;
-  flex-flow: row wrap;
-  align-items: center;
-}
-
-/* Add some margins for each label */
-.form-inline label {
-  margin: 5px 10px 5px 0;
-}
-
-/* Style the input fields */
-.form-inline input {
-  vertical-align: middle;
-  margin: 5px 10px 5px 0;
-  padding: 10px;
-  background-color: #fff;
-  border: 1px solid #ddd;
-}
-
-/* Style the submit button */
-.form-inline button {
-  padding: 10px 20px;
-  background-color: dodgerblue;
-  border: 1px solid #ddd;
-  color: white;
-}
-
-.form-inline button:hover {
-  background-color: royalblue;
-}
-
-/* Add responsiveness - display the form controls vertically instead of horizontally on screens that are less than 800px wide */
-@media (max-width: 800px) {
-  .form-inline input {
-    margin: 10px 0;
-  }
-
-  .form-inline {
-    flex-direction: column;
-    align-items: stretch;
-  }
-}
-</style>
-
-</head>
-<body>
-<!--  ============= NAVIGATION BAR ================
- -->
+	</head>
+	<body>
+	<!--  ============= NAVIGATION BAR ================
+	-->
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 		<a class="navbar-brand" href="../index.php">OASA logo</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -92,113 +45,187 @@
 		</div>
 	</nav>
 
-<!-- breadcrumbs -->
- <nav aria-label="breadcrumb">
-  <ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="in.html">Αρχική</a></li>
-    <li class="breadcrumb-item active" aria-current="page">Εύρεση διαδρομής</li>
-  </ol>
-</nav>   
-  
-  <div class="container">
-  <h3>Eπιλέξτε αφετηρία και προορισμό</h3>  
-  <form class="form-inline" action="/action_page.php">
-  <label for="email">Από:</label>
-  <input type="email" id="email" placeholder="Επιλέξτε σημείο εκκίνισης" name="email">
-  <label for="pwd">Πρός:</label>
-  <input type="password" id="pwd" placeholder="Επιλέξτε προορισμό" name="pswd">
-  <button type="submit">Εύρεση διαδρομής</button>
-</form>
-    
+	<!-- breadcrumbs -->
+	<nav aria-label="breadcrumb">
+	<ol class="breadcrumb">
+	<li class="breadcrumb-item"><a href="in.html">Αρχική</a></li>
+	<li class="breadcrumb-item active" aria-current="page">Εύρεση διαδρομής</li>
+	</ol>
+	</nav>   
 
-  </div>
+	<div class="container">
+	<h3>Eπιλέξτε αφετηρία και προορισμό</h3>  
+	<form class="form-inline" action="/action_page.php">
+		<label for="email">Από:</label>
+		<input type="email" id="email" placeholder="Επιλέξτε σημείο εκκίνισης" name="email">
+		<label for="pwd">Πρός:</label>
+		<input type="password" id="pwd" placeholder="Επιλέξτε προορισμό" name="pswd">
+		<button type="submit">Εύρεση διαδρομής</button>
+	</form>
+	</div>
 
-  <div class="accordion" id="accordionExample">
-  <div class="card">
-    <div class="card-header" id="headingOne">
-      <h2 class="mb-0">
-        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Collapsible Group Item #1
-        </button>
-      </h2>
-    </div>
+	<br/>
+	<br/>
 
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingTwo">
-      <h2 class="mb-0">
-        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Collapsible Group Item #2
-        </button>
-      </h2>
-    </div>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingThree">
-      <h2 class="mb-0">
-        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Collapsible Group Item #3
-        </button>
-      </h2>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-</div>
+	<?php
+		$query_for_routes = "select * 
+		from routes
+		where starting_point = '10η Καισαριανής' 
+		and destination = 'Λιμάνι Πειραιά'";
+
+		$result_routes = $conn->query($query_for_routes);
+		$routes_amount = $result_routes->num_rows;
+
+		
+
+		for ($i=0; $i < $routes_amount; $i++) {			
+			$result_routes-> data_seek($i);
+			$route_row = $result_routes->fetch_array(MYSQLI_ASSOC);
+
+			$query_for_steps = "select 
+				r.id, 
+				s.step_number, 
+				s.start, 
+				s.end, 
+				s.duration,
+				s.medium_type, 
+				s.medium_name, 
+				s.in_between_stops,
+				r.duration as total_duration, 
+				r.price as total_price
+			from routes as r, steps as s
+			where r.id = s.route_id
+			and s.route_id = ".$route_row['id']."
+			order by total_duration asc, s.step_number asc";
+
+			$result_steps = $conn->query($query_for_steps);
+			$steps_amount = $result_steps->num_rows;
+
+			// echo '<h3>Route #'.$route_row['id'].'</h3>';
+
+			$route_head = "";
+			$route_description = "";
+
+			for ($j=0; $j < $steps_amount; $j++) { 
+				$result_steps-> data_seek($j);
+				$step_row = $result_steps->fetch_array(MYSQLI_ASSOC);
+
+				if($route_head == ""){
+					$route_head = $step_row['medium_name'].'('.$step_row['medium_type'].')';
+				}
+				else{
+					$route_head = $route_head.'->'.$step_row['medium_name'].'('.$step_row['medium_type'].')';		
+				}
+
+				$route_description = '<p>'.$route_description
+				.$step_row['medium_name']
+				.'('.$step_row['medium_type'].')'.' : '
+				.$step_row['start']
+				.'-'
+				.$step_row['end']
+				.' ( '
+				.$step_row['duration']
+				.')'.'</p>'.'<br/>';
+
+				// echo '<p>Step #'.$step_row['step_number'].'</p>';
+			}
+
+			echo '<h3>'.$route_head.'</h3>';
+			echo $route_description;
+		}
+		
+
+	?>
+
+	<br/>
+	<br/>
+
+	<div class="accordion" id="accordionExample">
+	<div class="card">
+	<div class="card-header" id="headingOne">
+		<h2 class="mb-0">
+		<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+			Collapsible Group Item #1
+		</button>
+		</h2>
+	</div>
+
+	<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+		<div class="card-body">
+		Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+		</div>
+	</div>
+	</div>
+	<div class="card">
+	<div class="card-header" id="headingTwo">
+		<h2 class="mb-0">
+		<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+			Collapsible Group Item #2
+		</button>
+		</h2>
+	</div>
+	<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+		<div class="card-body">
+		Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+		</div>
+	</div>
+	</div>
+	<div class="card">
+	<div class="card-header" id="headingThree">
+		<h2 class="mb-0">
+		<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+			Collapsible Group Item #3
+		</button>
+		</h2>
+	</div>
+	<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+		<div class="card-body">
+		Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+		</div>
+	</div>
+	</div>
+	</div>
 
 
-<!-- FOOTER -->
-<br>
-<br>
-<br>
- 
- <footer class="site-footer">
-        <div class="containerr">
-        <div class="row" style="margin-left:20px;">
-          <div class="col-sm-12 col-md-6">
-            <h6>Επικοινωνία</h6>
-            <ul class="footer-links">
-              <li>Διευθυνση:Μετσόβου 15,Αθήνα 106 82</li>
-              <li>Τηλέφωνο:210-82.00.999</li>
-              <li>Φαξ:210-82.12.219</li>
-              <li>e-mail:oasa@oasa.gr</li>
-              <li>Τηλεφωνική Πληροφόρηση:11 185</li>
-            </ul>
-          </div>     
+	<!-- FOOTER -->
+	<br>
+	<br>
+	<br>
 
-          
+	<footer class="site-footer">
+		<div class="containerr">
+		<div class="row" style="margin-left:20px;">
+			<div class="col-sm-12 col-md-6">
+			<h6>Επικοινωνία</h6>
+			<ul class="footer-links">
+				<li>Διευθυνση:Μετσόβου 15,Αθήνα 106 82</li>
+				<li>Τηλέφωνο:210-82.00.999</li>
+				<li>Φαξ:210-82.12.219</li>
+				<li>e-mail:oasa@oasa.gr</li>
+				<li>Τηλεφωνική Πληροφόρηση:11 185</li>
+			</ul>
+			</div>     
 
-        </div>
-        <hr>
-      <div class="containerr">
-        <div class="row" style="margin-left:20px;">
-          <div class="col-md-8 col-sm-6 col-xs-12" >
-            <p class="copyright-text">Copyright &copy;2020 - University of Athens. All rights reserved.
-            </p>
-          </div>
+			
 
-          <div class="col-md-4 col-sm-6 col-xs-12">
-            <ul class="social-icons">
-              <li><a class="facebook" href="https://www.facebook.com/OASA.GR"><i class="fa fa-facebook"></i></a></li>
-              <li><a class="instagram" href="https://www.instagram.com/OASA.GR/"><i class="fa fa-instagram"></i></a></li>
-              <li><a class="youtube" href="https://www.youtube.com/channel/UC0XdkZnOHhRLc3NE9tm4NUQ"><i class="fa fa-youtube"></i></a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-</footer>
-</body>
+		</div>
+		<hr>
+		<div class="containerr">
+		<div class="row" style="margin-left:20px;">
+			<div class="col-md-8 col-sm-6 col-xs-12" >
+			<p class="copyright-text">Copyright &copy;2020 - University of Athens. All rights reserved.
+			</p>
+			</div>
+
+			<div class="col-md-4 col-sm-6 col-xs-12">
+			<ul class="social-icons">
+				<li><a class="facebook" href="https://www.facebook.com/OASA.GR"><i class="fa fa-facebook"></i></a></li>
+				<li><a class="instagram" href="https://www.instagram.com/OASA.GR/"><i class="fa fa-instagram"></i></a></li>
+				<li><a class="youtube" href="https://www.youtube.com/channel/UC0XdkZnOHhRLc3NE9tm4NUQ"><i class="fa fa-youtube"></i></a></li>
+			</ul>
+			</div>
+		</div>
+		</div>
+	</div>
+	</footer>
+	</body>
--- dummy.sql
+++ dummy.sql
@@ -1,12 +1,22 @@
-select r.id, r.duration, r.price, s.*
-from(
-    select * 
-    from routes
-    where starting_point = '10η Καισαριανής' 
-    and destination = 'Λιμάνι Πειραιά'
-    ) as r, steps as s
-where r.id = s.route_id
-order by r.duration asc, s.step_number asc 
+select 
+r.id as route_id, 
+s.step_number, 
+s.start, 
+s.end, 
+s.duration,
+s.medium_type, 
+s.medium_name, 
+s.in_between_stops,
+r.duration as total_duration, 
+r.price as total_price
+		from(
+			select * 
+			from routes
+			where starting_point = '10η Καισαριανής' 
+			and destination = 'Λιμάνι Πειραιά'
+			) as r, steps as s
+		where r.id = s.route_id
+		order by total_duration asc, s.step_number asc
 
 
 
