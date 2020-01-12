40490a765004441d49fff960a334edad6c9cfe56
GeorgeMitrakis <georgemitr96@hotmail.com>
2020-01-12 21:11:36 +0200

    finalized table in amea. added medium filter

--- amea/amea.php
+++ amea/amea.php
@@ -1,10 +1,117 @@
 <?php
 
-include '../db_connection/db_connection.php';
-session_start();
-include '../login_logout_button.php';
-include '../signup_profile_button.php';
+	include '../db_connection/db_connection.php';
+	session_start();
+	include '../login_logout_button.php';
+	include '../signup_profile_button.php';
 
+	function getStationType($medium_type){
+		switch ($medium_type) {
+			case 'Bus':
+				return 'Λεωφορείου';
+				break;
+			case 'Subway':
+				return 'Μετρό';
+				break;
+			case 'Railway':
+				return 'ΗΣΑΠ';
+				break;
+				
+			default:
+				return '';
+				break;
+		}
+	}
+
+	/////// medium type
+	if(isset($_GET['medium_type'])){ // 'all' , 'Bus', 'Railway' or 'Subway'
+		$medium_type = $_GET['medium_type'];
+	} else {
+		$medium_type = 'all';
+	}
+
+	switch ($medium_type) {
+		case 'Bus':
+			$dropdown_filter_header = 'Λεωφορεία/Τρόλλεϊ';
+			break;
+		
+		case 'Subway':
+			$dropdown_filter_header = 'Μετρό';
+			break;
+	
+		case 'Railway':
+			$dropdown_filter_header = 'ΗΣΑΠ';
+			break;
+			
+		default:
+			$dropdown_filter_header = 'Όλα';
+			break;
+	}
+
+	///// page number
+	if (isset($_GET['pageno'])) {
+		$pageno = $_GET['pageno'];
+	} else {
+		$pageno = 1;
+	}
+	$no_of_records_per_page = 10;
+	$offset = ($pageno-1) * $no_of_records_per_page;
+
+	if(strcmp($medium_type,'all') == 0){
+		$total_pages_sql = "SELECT COUNT(*) FROM amea_stations";
+	}
+	else{
+		$total_pages_sql = "SELECT COUNT(*) FROM amea_stations WHERE medium_type = '".$medium_type."'";
+	}
+
+	
+	$result = mysqli_query($conn,$total_pages_sql);
+	$total_rows = mysqli_fetch_array($result)[0];
+	$total_pages = ceil($total_rows / $no_of_records_per_page);
+	// echo $total_rows;
+
+	
+	////// table data
+	if(strcmp($medium_type,'all') == 0){
+		$query = "SELECT * FROM amea_stations ORDER BY district ASC, station ASC LIMIT $offset, $no_of_records_per_page ";
+	}
+	else{
+		$query = "SELECT * FROM amea_stations WHERE medium_type = '".$medium_type."' ORDER BY district ASC, station ASC LIMIT $offset, $no_of_records_per_page ";
+	}
+
+	$result = $conn->query($query);
+	$amount = $result->num_rows;
+	// if (!$amount) {
+	// 	printf("Error: %s\n", mysqli_error($conn));
+	// 	exit();
+	// }
+
+	$table_rows = "";
+	for ($i=0; $i < $amount; $i++) { 
+		$result->data_seek($i);
+		$row = $result->fetch_array(MYSQLI_ASSOC);
+		
+		$table_rows = $table_rows
+		."<tr>"
+		."<td>"
+		.$row['station']
+		."</td>
+		"
+		."<td>"
+		.$row['street']
+		."</td>
+		"
+		."<td>"
+		.$row['district']
+		."</td>
+		"
+		."<td>"
+		.getStationType($row['medium_type'])
+		."</td>
+		"."</tr>
+		"
+		;
+	}
 
 ?>
 
@@ -12,19 +119,20 @@ include '../signup_profile_button.php';
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>OASA.gr</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>OASA.gr</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" type="text/css" href="../amea/amea.css">
+	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="../amea/amea.css">
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-  <script src="amea.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	<script src="amea.js"></script>
 
 </head>
 
@@ -81,109 +189,94 @@ include '../signup_profile_button.php';
 		</div>
  -->
 
-	<div class="content">
+	<!-- <div class="content">
 		<button type="button" class="btn btn-outline-info btn-lg"><a href="../amea/info.php">Πληροφορίες</a></button>
 	</div>
-	<br>
-	<div class="bg-img">  
+	<br> -->
+	<!-- <div class="bg-img">   -->
 	
 		<div class="container table-container"> 
-			<table class="table" id="table">
-				<thead class="thead-dark">
-					<tr>
-						<th scope="col-sm">Στάση/Σταθμός</th>
-						<th scope="col-sm">Οδός</th>
-						<th scope="col-sm">Περιοχή</th>
-					</tr>
-				</thead>
-				<tbody>
-				<?php
-
-					if (isset($_GET['pageno'])) {
-						$pageno = $_GET['pageno'];
-					} else {
-						$pageno = 1;
-					}
-					$no_of_records_per_page = 10;
-					$offset = ($pageno-1) * $no_of_records_per_page;
-
-					$total_pages_sql = "SELECT COUNT(*) FROM amea_stations";
-					$result = mysqli_query($conn,$total_pages_sql);
-					$total_rows = mysqli_fetch_array($result)[0];
-					$total_pages = ceil($total_rows / $no_of_records_per_page);
-		
-					$query = "select * from amea_stations limit $offset, $no_of_records_per_page ";
-					$result = $conn->query($query);
-					$amount = $result->num_rows;
-
-					$table_rows = "";
-					for ($i=0; $i < $amount; $i++) { 
-						$result->data_seek($i);
-						$row = $result->fetch_array(MYSQLI_ASSOC);
-
-						$table_rows = $table_rows
-						."<tr>"
-						."<td>"
-						.$row['station']
-						."</td>
-						"
-						."<td>"
-						.$row['street']
-						."</td>
-						"
-						."<td>"
-						.$row['district']
-						."</td>
-						"."</tr>
-						"
-						;
-					}
-
-					echo $table_rows;
-
-					
-				?>
-				</tbody>
-			</table>
-		</div> 
-		<div class="container d-flex justify-content-center">
-			<nav>
-				<ul class="pagination">
-					<li class="page-item">
-						<a class="page-link" href="?pageno=1#table">First</a>
-					</li>
-					<li class="page-item <?php if($pageno <= 1){ echo 'disabled'; } ?>">
-						<a class="page-link" href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1); } ?>#table">Prev</a>
-					</li>
-
-					<?php
-						for ($i=1; $i <= $total_pages; $i++) { 
-							if( $i == $pageno){
-								echo '<li class="page-item active">
-								<a class="page-link" href="?pageno='.$i.'#table">'.$i .'</a>
+			<div class="row">
+				<div class="col">
+					<!-- <input type="text"> -->
+					<div class="card">
+						<div class="card-header">
+							<h5>Φίλτρα</h5>
+						</div>
+						<div class="card-body">
+							<div class="dropdown">
+								Ψάχνω στάσεις για: 
+								<button class="btn btn-secondary dropdown-toggle ml-2" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									<?php echo $dropdown_filter_header ?>
+								</button>
+								<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+									<a class="dropdown-item" href="?medium_type=all#table">Όλα</a>
+									<a class="dropdown-item" href="?medium_type=Bus#table">Λεωφορεία/Τρόλλεϊ</a>
+									<a class="dropdown-item" href="?medium_type=Subway#table">Μετρό</a>
+									<a class="dropdown-item" href="?medium_type=Railway#table">ΗΣΑΠ</a>
+								</div>
+							</div>	
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-9">
+					<div class="row d-flex justify-content-center">
+						<table class="table" id="table">
+							<thead class="thead-dark">
+								<tr>
+									<th scope="col-sm">Στάση/Σταθμός</th>
+									<th scope="col-sm">Οδός</th>
+									<th scope="col-sm">Δήμος</th>
+									<th scope="col-sm">Τύπος Στάσης</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php
+								echo $table_rows;						
+								?>
+							</tbody>
+						</table>
+					</div>
+					<div class="row d-flex justify-content-center">
+						<nav>
+							<ul class="pagination">
+								<li class="page-item">
+									<a class="page-link" href="?pageno=1&medium_type=<?php echo $medium_type; ?>#table">First</a>
 								</li>
-								';
-							}
-							else{
-								echo '<li class="page-item">
-								<a class="page-link" href="?pageno='.$i.'#table">'.$i .'</a>
+								<li class="page-item <?php if($pageno <= 1){ echo 'disabled'; } ?>">
+									<a class="page-link" href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1)."&medium_type=$medium_type"; } ?>#table">Prev</a>
 								</li>
-								';
-							}
-						}
-					?>
+								
+								<?php
+									for ($i=1; $i <= $total_pages; $i++) { 
+										if( $i == $pageno){
+											echo '<li class="page-item active">
+											<a class="page-link" href="?pageno='.$i.'&medium_type='.$medium_type.'#table">'.$i .'</a>
+											</li>
+											';
+										}
+										else{
+											echo '<li class="page-item">
+											<a class="page-link" href="?pageno='.$i.'&medium_type='.$medium_type.'#table">'.$i .'</a>
+											</li>
+											';
+										}
+									}
+								?>
 
-					<li class="page-item <?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
-						<a class="page-link" href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?>#table">Next</a>
-					</li>
-					<li class="page-item">
-						<a class="page-link" href="?pageno=<?php echo $total_pages; ?>#table">Last</a>
-					</li>
-				</ul>
-			</nav>
+								<li class="page-item <?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
+									<a class="page-link" href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1)."&medium_type=$medium_type"; } ?>#table">Next</a>
+								</li>
+								<li class="page-item">
+									<a class="page-link" href="?pageno=<?php echo $total_pages."&medium_type=$medium_type"; ?>#table">Last</a>
+								</li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
 		</div>
-
-	</div>
+	<!-- </div> -->
  
 
 </div>
